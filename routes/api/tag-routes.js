cef2e3b308c07675da7a491bae8265af38924bc2
Lantao home PC <lantaook@gmail.com>
2023-02-08 22:09:47 -0500

    fixed tag routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -25,16 +25,44 @@ router.get("/:id", async (req, res) => {
   }
 });
 
-router.post("/", (req, res) => {
+router.post("/", async (req, res) => {
   // create a new tag
+  try {
+    const newTag = await Tag.create(req.body);
+    res.status(200).json(newTag);
+  } catch (err) {
+    res.status(400).json(err)
+  }
 });
 
-router.put("/:id", (req, res) => {
+router.put("/:id",async (req, res) => {
   // update a tag's name by its `id` value
+  try {
+    const update = await Tag.update({
+      tag_name: req.body.tag_name
+    }, {
+      where: {
+        id: req.params.id
+      }
+    });
+    res.status(200).json(update)
+  } catch (err) {
+    res.status(400).json(err)
+  }
 });
 
-router.delete("/:id", (req, res) => {
+router.delete("/:id", async (req, res) => {
   // delete on tag by its `id` value
+  try {
+    const tag = await Tag.destroy({
+      where: {
+        id:req.params.id
+      }
+    });
+    res.status(200).json(tag);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 module.exports = router;
